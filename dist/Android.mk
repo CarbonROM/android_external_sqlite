96e3b71930cc84804862ab200b8c51fead532c2d
Alex Naidis <alex.naidis@linux.com>
2016-12-27 00:39:10 +0100

    sqlite: Disable SDCLANG
    * Disable SDCLANG, since sqlite
      is not friendly to these optimizations.
    
    Change-Id: Ide1f0ca65588c1e68df66fd0ebdb7e00533f2801
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- dist/Android.mk
+++ dist/Android.mk
@@ -51,6 +51,8 @@ LOCAL_SRC_FILES := $(common_src_files)
 LOCAL_CFLAGS += $(device_sqlite_flags)
 LOCAL_CFLAGS_linux += $(minimal_linux_flags)
 
+LOCAL_SDCLANG := false
+
 LOCAL_SHARED_LIBRARIES := libdl
 
 LOCAL_MODULE:= libsqlite
@@ -95,6 +97,8 @@ ifneq ($(SDK_ONLY),true)  # SDK doesn't need device version of sqlite3
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := shell.c
+
+LOCAL_SDCLANG := false
 
 LOCAL_C_INCLUDES := \
     $(LOCAL_PATH)/../android \
